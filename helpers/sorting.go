9f881c8e5175669f97af6b70f0e5b7d37dc2f375
allie-yam <151028371+allie-yam@users.noreply.github.com>
2024-03-20 20:54:38 +0800

    temporary commit

--- helpers/sorting.go
+++ helpers/sorting.go
@@ -11,25 +11,27 @@ type Restaurant struct {
 	WeightedScore float64
 }
 
-func GetWeightedScores(restaurants []Restaurant) []Restaurant {
-	cuisineWeight := 0.4
-	distanceWeight := 0.3
-	ratingWeight := 0.2
-	priceStarsWeight := 0.1
-
+func SortByWeightedScore(restaurants []Restaurant) {
 	maxDistance, maxRating, maxPriceStars := getMaxEverything(restaurants)
-	// normalize to a value between 0-1
-	// higher values are better
-	for i := range restaurants {
-		restaurants[i].WeightedScore += distanceWeight * (1 - restaurants[i].Distance/maxDistance)
-		restaurants[i].WeightedScore += ratingWeight * (restaurants[i].Rating / maxRating)
-		restaurants[i].WeightedScore += priceStarsWeight * (float64(restaurants[i].PricingStars) / float64(maxPriceStars))
-	}
 
 	sort.Slice(restaurants, func(i, j int) bool {
-		return restaurants[i].WeightedScore > restaurants[j].WeightedScore
+		leftScore := GetWeightedScore(restaurants[i], maxDistance, maxRating, float64(maxPriceStars))
+		rightScore := GetWeightedScore(restaurants[j], maxDistance, maxRating, float64(maxPriceStars))
+		return leftScore < rightScore
 	})
-	return restaurants
+}
+
+func GetWeightedScore(restaurant Restaurant, maxDistance, maxRating, maxPriceStars float64) float64 {
+	distanceWeight := 0.4
+	ratingWeight := 0.3
+	priceStarsWeight := 0.3
+
+	var weightedScore float64
+	weightedScore += distanceWeight * (1 - restaurant.Distance/maxDistance)
+	weightedScore += ratingWeight * (restaurant.Rating / maxRating)
+	weightedScore += priceStarsWeight * (float64(restaurant.PricingStars) / maxPriceStars)
+
+	return weightedScore
 }
 
 func getMaxEverything(restaurants []Restaurant) (float64, float64, int) {

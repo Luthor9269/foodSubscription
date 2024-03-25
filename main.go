0985bc3f8176fc64792d2f105ef94be81954786a
allie-yam <151028371+allie-yam@users.noreply.github.com>
2024-03-26 00:41:15 +0800

    temporary commit

--- main.go
+++ main.go
@@ -2,12 +2,19 @@ package main
 
 import (
 	databaseConnection "github.com/Luthor9269/foodSubscription.git/models"
+	"github.com/joho/godotenv"
+	"log"
+	"net/http"
 )
 
 func main() {
 	db := databaseConnection.ConnectToDB()
 	defer db.Close()
-
+	http.ListenAndServe(":8080", nil)
 	//start server
 	// http.ListenAndServe(":8080", nil)
+	err := godotenv.Load(".env")
+	if err != nil {
+		log.Fatalf("Error loading .env file: %s", err)
+	}
 }
